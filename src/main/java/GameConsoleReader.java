bea592dca63721143a3e983f579b22f9762bf028
Shruti <in-shruti.singh@Shrutis-MacBook-Pro-2.local>
2018-12-11 18:38:48 +0530

    commenting scanner functionality

--- src/main/java/GameConsoleReader.java
+++ src/main/java/GameConsoleReader.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class GameConsoleReader implements GameInputReader {
     @Override
     public void input(Board board, Symbol symbol) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter input (Symbol->(xcoordinate, ycoordinate)");
-        String input = in.next();
-        assignToBoard(board, symbol);
+//        Scanner in = new Scanner(System.in);
+//        System.out.println("Enter input (Symbol->(xcoordinate, ycoordinate)");
+//        String input = in.next();
+//        assignToBoard(board, symbol);
     }
     //TODO process and validate
     public void assignToBoard(Board board, Symbol symbol){
